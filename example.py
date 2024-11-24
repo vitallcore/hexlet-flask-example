9719f918fecc9f9950176749f0fdb1104efb7170
vitallcore <vitaslipknot@gmail.com>
2024-11-24 20:14:56 +0600

    flask

--- example.py
+++ example.py
@@ -33,3 +33,17 @@ def html():
 @app.errorhandler(404)
 def not_found(error):
     return 'Oops!', 404
+
+
+@app.route('/foo')
+def foo():
+    response = make_response('foo')
+    # Устанавливаем заголовок
+    response.headers['X-Parachutes'] = 'parachutes are cool'
+    # Меняем тип ответа
+    response.mimetype = 'text/plain'
+    # Задаем статус
+    response.status_code = 418
+    # Устанавливаем cookie
+    response.set_cookie('foo', 'bar')
+    return response

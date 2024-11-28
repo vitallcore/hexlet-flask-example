15bffada033ac19f17c8698eb719b6bcb1c6c138
vitallcore <vitaslipknot@gmail.com>
2024-11-28 18:02:50 +0600

    flask

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, render_template
+from flask import Flask, request, make_response, render_template, redirect, flash, url_for, get_flashed_messages
 
 # Это callable WSGI-приложение
 app = Flask(__name__)
@@ -73,3 +73,29 @@ def get_user(id):
 @app.route('/users')
 def get_users():
     return render_template('users/index.html', users=users)
+
+
+app = Flask(__name__)
+# устанавливаем секретный ключ для работы с сессиями
+app.secret_key = "secret_key"
+
+
+@app.post('/foo')
+def foo_post():
+    # Добавление флеш-сообщения.
+    # Оно станет доступным только на следующий HTTP-запрос.
+    # 'success' — тип флеш-сообщения. Используется при выводе для форматирования.
+    # Например, можно ввести тип success и отражать его зеленым цветом. На Хекслете такого много.
+    flash('This is a message', 'success')
+    return redirect('/bar')
+
+
+@app.get('/bar')
+def bar_index():
+    # Извлечение flash-сообщений, которые установлены на предыдущем запросе
+    messages = get_flashed_messages(with_categories=True)
+    print(messages)  # => [('success', 'This is a message')]
+    return render_template(
+        'bar.html',
+        messages=messages,
+    )
